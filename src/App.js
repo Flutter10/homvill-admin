cd98c12c0b303389b7a180ff94f6c0c561e7dcdb
Flutter <Flutter10@flyweis.technology>
2025-08-13 11:19:00 +0530

    pushes

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import './index.css'
 import LoginPage from './components/Login'
 import Signup from './components/Signup'
 import Forgot from './components/Forgot/Forgot'
+import Otpverification from './components/Forgot/Otpverification'
+import NewPassword from './components/Forgot/NewPassword'
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 
 const App = () => {
@@ -13,6 +15,8 @@ const App = () => {
         <Route path="/" element={<LoginPage />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/forgot" element={<Forgot />} />
+        <Route path="/otp" element={<Otpverification />} />
+        <Route path="/newpassword" element={<NewPassword />} />
         </Routes>
     </>
   )
